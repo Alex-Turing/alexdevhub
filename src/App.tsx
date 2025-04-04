7141bb322c3fc87f9cdfe9fb1a1bfc8f395e3821
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-04-04 10:19:20 -0400

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import githubService from './services/GithubService';
 import NavigationBar from './components/NavigationBar';
 import styled from 'styled-components';
