8d5faca97588f64e845009173fdc1e3787225ccc
Alex-Turing <alexanderhernandez0684@gmail.com>
2025-04-04 09:51:16 -0400

    config details on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/alexdevhub/", // Add this line (repo name as base path)
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
